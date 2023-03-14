4039726e5c469e87bda601008ceca4152ca35e37
Fabio Luiz <fabiolfsjr@gmail.com>
2023-03-14 16:49:39 -0300

    Cria funções 'validarCPF' e 'digitoVerificador'

--- Lab02/src/Cliente.java
+++ Lab02/src/Cliente.java
@@ -52,4 +52,36 @@ public class Cliente{
     public void setEndereco(String endereco){
         this.endereco = endereco;
     }
+
+    public int digitoVerificador(String cpf, int caso){
+        int soma = 0, aux = 0, digito;
+
+        if(caso == 2)
+            aux = 1;
+        for(int i = 10 + aux; i >= 2; i--)
+            soma += (cpf.charAt(10 + aux - i) - '0') * i;
+        
+        if((soma % 11 == 0) || (soma % 11 == 1))
+            digito = 0;
+        else
+            digito = 11 - (soma % 11);
+        
+        return digito;
+    }
+
+    public boolean validarCPF(String cpf){
+        int digito_1, digito_2;
+
+        cpf.replaceAll(".", "");
+        cpf.replaceAll("-", "");
+        if(cpf.length() != 11)
+            return false;
+        
+        digito_1 = digitoVerificador(cpf, 1);
+        digito_2 = digitoVerificador(cpf, 2);
+        if((digito_1 != cpf.charAt(9) - '0') || (digito_2 != cpf.charAt(10) - '0'))
+            return false;
+
+        return true;
+    }
 }
