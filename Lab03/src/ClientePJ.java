95ef9b641b60964ea2be56a356122ca0fc8bc37e
Fabio Luiz <fabiolfsjr@gmail.com>
2023-04-04 16:02:02 -0300

    Cria métodos para validar o CNPJ

--- Lab03/src/ClientePJ.java
+++ Lab03/src/ClientePJ.java
@@ -30,5 +30,37 @@ public class ClientePJ extends Cliente{
             ", dataFundacao='" + getDataFundacao() + "'" +
             "}";
     }
-    
+
+    public int digitoVerificador(String cpf, int digVerificador){
+        int soma = 0, aux = 0, digito;
+
+        if(digVerificador == 2)
+            aux = 1;
+        for(int i = 5; i >= 2; i--)
+            soma += (cpf.charAt(5 - i) - '0') * i;
+        for(int i = 9 + aux; i >= 2; i--)
+            soma += (cpf.charAt(14 + aux - i) - '0') * i;
+        
+        if(soma % 11 <= 1)
+            digito = 0;
+        else
+            digito = 11 - (soma % 11);
+        
+        return digito;
+    }
+
+    public boolean validarCNPJ(String cnpj){
+        int digito_1, digito_2;
+
+        cnpj = cnpj.replaceAll("\\.|-", "");
+        if(cnpj.length() != 14)
+            return false;
+        
+        digito_1 = digitoVerificador(cnpj, 1);
+        digito_2 = digitoVerificador(cnpj, 2);
+        if((digito_1 != cnpj.charAt(12) - '0') || (digito_2 != cnpj.charAt(13) - '0'))
+            return false;
+
+        return true;
+    }
 }
