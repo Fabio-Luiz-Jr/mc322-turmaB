3129f536436d5265df8f6e958c0c63c4d7b78a1d
Fabio Luiz <fabiolfsjr@gmail.com>
2023-06-01 12:38:29 -0300

    Cria método "atualizarFrota"

--- Lab05/src/ClientePJ.java
+++ Lab05/src/ClientePJ.java
@@ -19,6 +19,12 @@ public class ClientePJ extends Cliente{
     public void setDataFundacao(Date dataFundacao){this.dataFundacao = dataFundacao;}
     public ArrayList<Frota> getListaFrota(){return listaFrota;}
     public void setListaFrota(ArrayList<Frota> listaFrota){this.listaFrota = listaFrota;}
+    private Frota getFrota(Frota frota){
+        for(int i = 0; i < getListaFrota().size(); i++)
+            if(Objects.equals(this.listaFrota.get(i), frota))
+                return this.listaFrota.get(i);
+        return null;
+    }
     //#endregion
     @Override
     public String toString(){
@@ -32,6 +38,21 @@ public class ClientePJ extends Cliente{
         if(getListaFrota().contains(frota))
             return false;
         this.listaFrota.add(frota);
+        return true;
+    }
+
+    public boolean atualizarFrota(Frota frota){
+        if(getListaFrota().contains(frota)){
+            this.listaFrota.remove(frota);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean atualizarFrota(Frota frota, Veiculo veiculo){
+        if(getFrota(frota) == null)
+            return false;
+        getFrota(frota).addVeiculo(veiculo);
         return true;
     }
 }
