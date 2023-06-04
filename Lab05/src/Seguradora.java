afd9446744f894cddf24f73de0b20c54a4ff1ba7
Fabio Luiz <fabiolfsjr@gmail.com>
2023-06-03 22:57:19 -0300

    Implementa método "gerarSeguro"

--- Lab05/src/Seguradora.java
+++ Lab05/src/Seguradora.java
@@ -1,24 +1,25 @@
 import java.text.Normalizer;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Objects;
 
 public class Seguradora{
     private final String cnpj;
     private String nome;
     private String telefone;
-    private String email;
     private String endereco;
-    private ArrayList<Sinistro> listaSinistros;
+    private String email;
     private ArrayList<Cliente> listaClientes;
+    private ArrayList<Seguro> listaSeguros;
 
     public Seguradora(String cnpj, String nome, String telefone, String email, String endereco){
         this.cnpj = cnpj;
         this.nome = nome;
         this.telefone = telefone;
-        this.email = email;
         this.endereco = endereco;
-        this.listaSinistros = new ArrayList<Sinistro>();
+        this.email = email;
         this.listaClientes = new ArrayList<Cliente>();
+        this.listaSeguros = new ArrayList<Seguro>();
     }
     //#region Getters e setters
     public String getCnpj(){return this.cnpj;}
@@ -26,43 +27,30 @@ public class Seguradora{
     public void setNome(String nome){this.nome = nome;}
     public String getTelefone(){return this.telefone;}
     public void setTelefone(String telefone){this.telefone = telefone;}
-    public String getEmail(){return this.email;}
-    public void setEmail(String email){this.email = email;}
     public String getEndereco(){return this.endereco;}
     public void setEndereco(String endereco){this.endereco = endereco;}
-    public ArrayList<Sinistro> getListaSinistros(){return listaSinistros;}
-    public void setListaSinistros(Sinistro sinistro){this.listaSinistros.add(sinistro);}
+    public String getEmail(){return this.email;}
+    public void setEmail(String email){this.email = email;}
     public ArrayList<Cliente> getListaClientes(){return listaClientes;}
     public void setListaClientes(ArrayList<Cliente> listaClientes){this.listaClientes = listaClientes;}
+    public ArrayList<Seguro> getListaSeguros(){return listaSeguros;}
+    public void setListaSeguros(ArrayList<Seguro> listaSeguros){this.listaSeguros = listaSeguros;}
+    public Cliente getCliente(String nome){
+        for(int i = listarClientes("todos").size() - 1; i >= 0; i--)
+            if(Objects.equals(listaClientes.get(i).getNome(), nome))
+                return listaClientes.get(i);
+        return null;
+    }
     //#endregion
     @Override
     public String toString(){
         return "CNPJ: " + getCnpj() + 
                "\nNome: " + getNome() + 
                "\nTelefone: " + getTelefone() + 
-               "\nEmail: " + getEmail() + 
                "\nEndereço: " + getEndereco() + 
-               "\nLista de sinistros: " + getListaSinistros() + 
-               "\nLista de clientes: " + getListaClientes();
-    }
-
-    public boolean cadastrarCliente(Cliente cliente){
-        if(listaClientes.contains(cliente))
-            return false;
-        this.listaClientes.add(cliente);
-        return true;
-    }
-
-    public boolean removerCliente(String cliente){
-        int index = 0;
-        for(Cliente c: listaClientes){
-            if(Objects.equals(c.getNome(), cliente)){
-                listaClientes.remove(index);
-                return true;
-            }
-            index++;
-        }
-        return false;
+               "\nEmail: " + getEmail() + 
+               "\nLista de clientes: " + getListaClientes() + 
+               "\nLista de seguros: " + getListaSeguros();
     }
 
     public ArrayList<Cliente> listarClientes(String tipoCliente){
@@ -91,11 +79,38 @@ public class Seguradora{
         return listarClientes("todos");
     }
 
-    public Cliente getCliente(String nome){
-        for(int i = listarClientes("todos").size() - 1; i >= 0; i--)
-            if(Objects.equals(listaClientes.get(i).getNome(), nome))
-                return listaClientes.get(i);
-        return null;
+    public boolean gerarSeguro(Seguradora seguradora, Date dataInicio, Date dataFim, double valorMensal, Veiculo veiculo, ClientePF clientePF){
+        if(!Validacao.validaCPF(clientePF.getCpf()))
+            return false;
+        this.listaSeguros.add(new SeguroPF(dataInicio, dataFim, seguradora, valorMensal, veiculo, clientePF));
+        return true;
     }
+
+    public boolean gerarSeguro(Seguradora seguradora, Date dataInicio, Date dataFim, double valorMensal, Frota frota, ClientePJ clientePJ){
+        if(!Validacao.validaCNPJ(clientePJ.getCnpj()))
+            return false;
+        this.listaSeguros.add(new SeguroPJ(dataInicio, dataFim, seguradora, valorMensal, frota, clientePJ));
+        return true;
+    }
+
+    public boolean cadastrarCliente(Cliente cliente){
+        if(listaClientes.contains(cliente))
+            return false;
+        this.listaClientes.add(cliente);
+        return true;
+    }
+
+    public boolean removerCliente(String cliente){
+        int index = 0;
+        for(Cliente c: listaClientes){
+            if(Objects.equals(c.getNome(), cliente)){
+                listaClientes.remove(index);
+                return true;
+            }
+            index++;
+        }
+        return false;
+    }
+
 
 }
